c96dc79bed2b6e7f307e9da7ed145030cc964473
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-11-06 17:43:32 -0300

    Add Mailpit setup for local email testing and implement event reminder notifications

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(SendEventReminders::class)->daily();
+Schedule::command(SendEventReminders::class)->everyFifteenSeconds();
